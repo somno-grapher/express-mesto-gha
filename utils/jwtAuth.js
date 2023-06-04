448aa77c5d287c4a2626425515740cc9a54008f8
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-06-04 11:42:29 +0300

    feat: provide token expire term

--- utils/jwtAuth.js
+++ utils/jwtAuth.js
@@ -4,6 +4,10 @@ const SECRET_KEY = 'super_secret';
 
 const checkToken = (token) => jwt.verify(token, SECRET_KEY);
 
-const signToken = (payload) => jwt.sign(payload, SECRET_KEY);
+const signToken = (payload) => jwt.sign(
+  payload,
+  SECRET_KEY,
+  { expiresIn: '7d' },
+);
 
 module.exports = { checkToken, signToken };
