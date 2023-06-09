d033fa51158861d809449921fd25647773c19367
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-06-09 20:40:31 +0300

    fix: route not found error

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const cardRouter = require('./cards');
 const usersController = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validateUserBody } = require('../middlewares/validate');
+const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/signup', validateUserBody, usersController.createUser);
 router.use('/signin', validateUserBody, usersController.login);
@@ -13,8 +14,8 @@ router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
-router.use((req, res) => {
-  res.status(404).send({ message: 'Маршрут не найден' });
+router.use((req, res, next) => {
+  next(new NotFoundError('Маршрут не найден'));
 });
 
 module.exports = router;
