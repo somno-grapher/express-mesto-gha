7adf907f4fd43765a69cb9d2f83d00952d9c1df2
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-06-10 12:40:43 +0300

    refactor: create handleError func

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { errors } = require('celebrate');
 
 const { PORT, DB_URL } = require('./config');
 const router = require('./routes');
-const STATUS_CODES = require('./utils/consts');
+const handleError = require('./utils/handleError');
 
 const app = express();
 
@@ -17,17 +17,7 @@ app.use(router);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = STATUS_CODES.INTERNAL_SERVER_ERROR, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === STATUS_CODES.INTERNAL_SERVER_ERROR
-        ? 'Внутренняя ошибка сервера'
-        : message,
-    });
-  next();
-});
+app.use(handleError);
 
 mongoose.connect(DB_URL)
   .then(() => {
