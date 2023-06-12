46666e2a05e83a6341400800e804ae227e704a12
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-06-12 10:58:16 +0300

    fix: set validateUserBodyOnProfileUpdate and validateUserBodyOnAvatarUpdate fields as required

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -17,14 +17,14 @@ const validateUserBody = celebrate({
 
 const validateUserBodyOnProfileUpdate = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 });
 
 const validateUserBodyOnAvatarUpdate = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().custom(validateURL),
+    avatar: Joi.string().required().custom(validateURL),
   }),
 });
 
